bf5512ca89b2eebbfe31e24bbdcb763a2ccbcdde
Abhishek Agrawal <aagrawal207@gmail.com>
2018-03-07 23:29:28 +0530

    Minor bug resolved

--- SearchFile.java
+++ SearchFile.java
@@ -7,9 +7,11 @@ import java.util.*;
 
 public class SearchFile {
   public static String returnFileAddress(int fileKey, String IPAddress, int port) throws Exception {
+    if(ObtainSHA.SHA1(IPAddress + ":" + port) == fileKey)
+      return (IPAddress + ":" + port);
     System.out.println("Peer IP: " + IPAddress + ", port: " + port);
     Socket askForFileAddressServer = new Socket(InetAddress.getByName(IPAddress), port);
-    System.out.println("XXXXXXXXXXXXX returnFileAddress() XXXXXXXXXXXXX");
+    System.out.println("XXXXXXXXXXXXX returnFileAddress() of " + fileKey + " XXXXXXXXXXXXX");
     BufferedReader br = new BufferedReader(new InputStreamReader(askForFileAddressServer.getInputStream()));
     OutputStream os = askForFileAddressServer.getOutputStream();
     os.write(("SendFileAddress\n").getBytes());
